23c3ec48497782bbc91d829e1c8a682502360ab9
Thai Thien <ttpro1995@yahoo.com.vn>
2019-08-24 21:19:22 +0700

    wip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from args_util import real_args_parse
 from data_flow import get_train_val_list, get_dataloader
+from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
 if __name__ == "__main__":
     args = real_args_parse()
     DATA_PATH = args.input
@@ -8,5 +9,7 @@ if __name__ == "__main__":
     train_list, val_list = get_train_val_list(DATA_PATH)
     # create data loader
     train_loader, test_loader = get_dataloader(train_list, val_list)
+
+
 
     print(args)
