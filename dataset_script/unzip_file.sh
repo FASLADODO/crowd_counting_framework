f2106e700b6f6174d4dd276f25ec6f3d9ff239bb
thient <thient@vng,com.vn>
2019-10-04 14:42:51 +0700

    shell script

--- dataset_script/unzip_file.sh
+++ dataset_script/unzip_file.sh
@@ -5,5 +5,7 @@
  # mv ucf-cc-50-with-people-density-map.zip  ../data/ ; cd ../data/ ;unzip ucf-cc-50-with-people-density-map.zip
 
 mv perspective-shanghaitech.zip data/
+mv shanghaitech-with-people-density-map.zip data/
 cd data/
 unzip perspective-shanghaitech.zip
+unzip shanghaitech-with-people-density-map.zip data/
