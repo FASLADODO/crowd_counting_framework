a144c83e69e4363311fa74f1c091d1a3f834ad8a
Thai Thien <tthien@apcs.vn>
2020-02-29 00:16:41 +0700

    fix  error: argument --decay: invalid float value: '5*1e-4'

--- train_script/attn_can_adcrowdnet/train_server_31_epoch_shA_freeze_vgg.sh
+++ train_script/attn_can_adcrowdnet/train_server_31_epoch_shA_freeze_vgg.sh
@@ -10,6 +10,6 @@ CUDA_VISIBLE_DEVICES=5 nohup python train_attn_can_adcrowdnet_freeze_vgg.py  \
 --task_id attn_can_adcrowdnet_default_shtA_33_freeze_vgg_2  \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
 --lr 1e-4 \
---decay 5*1e-4 \
+--decay 5e-4 \
 --datasetname shanghaitech \
 --epochs 33 > logs/attn_can_adcrowdnet_default_shtA_33_freeze_vgg_2.log  &
