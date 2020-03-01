eef3995f63a631e0ec5d92e31f5d7db27fd04401
Thai Thien <tthien@apcs.vn>
2020-03-01 12:17:55 +0700

    print train loader len

--- train_attn_can_adcrowdnet_simple.py
+++ train_attn_can_adcrowdnet_simple.py
@@ -37,6 +37,7 @@ if __name__ == "__main__":
     # create data loader
     train_loader, val_loader, test_loader = get_dataloader(train_list, val_list, test_list, dataset_name=dataset_name)
 
+    print("len train_loader ", len(train_loader))
 
     # model
     model = AttnCanAdcrowdNetSimpleV3()
