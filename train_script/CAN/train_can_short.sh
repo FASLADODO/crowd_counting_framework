1ff1cae8b9fc75fddc4778db804d1469713e55b2
Thai Thien <tthien@apcs.vn>
2020-03-01 10:25:29 +0700

    well, fix can train script actually
    last commit fix wrong script

--- train_script/CAN/train_can_short.sh
+++ train_script/CAN/train_can_short.sh
@@ -11,7 +11,7 @@
 CUDA_VISIBLE_DEVICES=4 nohup python train_context_aware_network.py  \
 --task_id can_default_shtA_t1  \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
---lr 1e-4 \
+--lr 1e-5 \
 --decay 5e-4 \
 --datasetname shanghaitech_keepfull \
 --epochs 46 > logs/can_default_shtA_t1.log  &
