0e9d372b9ad60b32939f1e558b2a59fc7d518fa2
Thai Thien <tthien@apcs.vn>
2020-03-02 23:23:55 +0700

    fix script

--- train_script/my_simple1/my_simple_v3_load_t3.sh
+++ train_script/my_simple1/my_simple_v3_load_t3.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=4 nohup python train_attn_can_adcrowdnet_simple.py  \
 --task_id simple_v3_t3  \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
---load_model saved_model/simple_v3_t1/simple_v3_t2_checkpoint_64800.pth  \
+--load_model saved_model/simple_v3_t2/simple_v3_t2_checkpoint_64800.pth  \
 --lr 1e-7 \
 --decay 5e-4 \
 --datasetname shanghaitech_keepfull \
