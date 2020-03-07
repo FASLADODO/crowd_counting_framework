aa331331b12e5b454d372a550524b30a4bebe706
Thai Thien <tthien@apcs.vn>
2020-03-08 01:32:06 +0700

    fix script

--- train_script/CCNN/ccnn_v3_t1.sh
+++ train_script/CCNN/ccnn_v3_t1.sh
@@ -4,4 +4,4 @@ CUDA_VISIBLE_DEVICES=6 nohup python train_compact_cnn.py  \
 --lr 1e-5 \
 --decay 5e-5 \
 --datasetname shanghaitech_keepfull \
---epochs 401 > logs/ccnn_v1_t1.log  &
+--epochs 401 > logs/ccnn_v3_t1.log  &
