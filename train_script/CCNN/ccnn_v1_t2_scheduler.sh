631c37c068377706ae2cad4513baeea7e62ae0b5
Thai Thien <tthien@apcs.vn>
2020-03-11 23:59:24 +0700

    fix script name

--- train_script/CCNN/ccnn_v1_t2_scheduler.sh
+++ train_script/CCNN/ccnn_v1_t2_scheduler.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=3  HTTPS_PROXY="http://10.30.58.36:81" nohup python train_custom_compact_cnn_lrscheduler.py  \
+CUDA_VISIBLE_DEVICES=3  HTTPS_PROXY="http://10.30.58.36:81" nohup python train_compact_cnn_lrscheduler.py  \
 --task_id ccnn_v1_t2_scheduler  \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
 --lr 1e-4 \
