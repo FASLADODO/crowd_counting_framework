85dcfe49ecbcef2ccb1bb70b2a1b898440180286
Thai Thien <tthien@apcs.vn>
2020-03-15 00:43:00 +0700

    ccnn_v5_t2shb

---
+++ train_script/CCNN/ccnn_v5_t2shb.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=5 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_compact_cnn.py  \
+--task_id ccnn_v5_t2_shb  \
+--note "train CompactCNNV2 with shb on batchsize of 5"  \
+--input /data/rnd/thient/thient_data/ShanghaiTech/part_B  \
+--lr 1e-4 \
+--decay 0 \
+--datasetname shanghaitech_keepfull \
+--batch_size 5 \
+--epochs 502 > logs/ccnn_v5_t2_shb.log  &
