44fb230848afb391fa4efa4e412d3def3e32965a
Thai Thien <tthien@apcs.vn>
2020-03-14 18:29:26 +0700

    gpu 5

--- train_script/CCNN/ccnn_v5_t2.sh
+++ train_script/CCNN/ccnn_v5_t2.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=6 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_compact_cnn.py  \
+CUDA_VISIBLE_DEVICES=5 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_compact_cnn.py  \
 --task_id ccnn_v5_t2  \
 --note "train CompactCNNV2 (fixed by adding 1 layer of cnn) with fixed lr 1e-5 no decay on keepfull and crop dataset part_A"  \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
