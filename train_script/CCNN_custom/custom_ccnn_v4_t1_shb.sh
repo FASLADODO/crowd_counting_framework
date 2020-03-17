57017c52dd5788f481173cefae2aab56dd15bad4
Thai Thien <tthien@apcs.vn>
2020-03-18 00:59:13 +0700

    batch_size 1

--- train_script/CCNN_custom/custom_ccnn_v4_t1_shb.sh
+++ train_script/CCNN_custom/custom_ccnn_v4_t1_shb.sh
@@ -5,5 +5,16 @@ CUDA_VISIBLE_DEVICES=1 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_cu
 --lr 1e-5 \
 --decay 1e-5 \
 --datasetname shanghaitech_keepfull \
---batch_size 4 \
+--batch_size 1 \
 --epochs 200 > logs/custom_ccnn_v4_t1_shb.log  &
+
+
+#python train_custom_compact_cnn.py  \
+#--task_id custom_ccnn_v4_t1_shb  \
+#--note "defromable"  \
+#--input /data/ShanghaiTech/part_B/  \
+#--lr 1e-5 \
+#--decay 1e-5 \
+#--datasetname shanghaitech_keepfull \
+#--batch_size 1 \
+#--epochs 200
