44c6dea2b2e5017f8c6f3bf87136220d6e9def42
thient <thient@vng.com.vn>
2020-03-18 10:16:49 +0700

    increase epoch ... because we are loading old model

--- train_script/CCNN_custom/custom_ccnn_v4_t1_shb.sh
+++ train_script/CCNN_custom/custom_ccnn_v4_t1_shb.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=2 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_cu
 --datasetname shanghaitech_keepfull \
 --load_model saved_model/custom_ccnn_v4_t1_shb/custom_ccnn_v4_t1_shb_checkpoint_80000.pth \
 --batch_size 1 \
---epochs 50 > logs/custom_ccnn_v4_t1_c1_shb.log  &
+--epochs 200 > logs/custom_ccnn_v4_t1_c1_shb.log  &
 
 
 #python train_custom_compact_cnn.py  \
