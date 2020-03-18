f2bd926e2db7f73065433ad6702c6edee31076b3
thient <thient@vng.com.vn>
2020-03-18 10:12:03 +0700

    continue train with higher lr

--- train_script/CCNN_custom/custom_ccnn_v4_t1_shb.sh
+++ train_script/CCNN_custom/custom_ccnn_v4_t1_shb.sh
@@ -1,12 +1,13 @@
 CUDA_VISIBLE_DEVICES=2 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_custom_compact_cnn.py  \
---task_id custom_ccnn_v4_t1_shb  \
+--task_id custom_ccnn_v4_t1_c1_shb  \
 --note "defromable"  \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_B  \
---lr 1e-5 \
---decay 1e-5 \
+--lr 1e-4 \
+--decay 1e-4 \
 --datasetname shanghaitech_keepfull \
+--load_model saved_model/custom_ccnn_v4_t1_shb/custom_ccnn_v4_t1_shb_checkpoint_80000.pth
 --batch_size 1 \
---epochs 100 > logs/custom_ccnn_v4_t1_shb.log  &
+--epochs 50 > logs/custom_ccnn_v4_t1_c1_shb.log  &
 
 
 #python train_custom_compact_cnn.py  \
