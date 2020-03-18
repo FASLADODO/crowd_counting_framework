c16871fb2cc035e168d62df9c5d4401bc465df95
Thai Thien <tthien@apcs.vn>
2020-03-19 00:27:45 +0700

    typo again

--- models/__init__.py
+++ models/__init__.py
@@ -6,5 +6,5 @@ from .can_adcrowdnet import CanAdcrowdNet
 from .attn_can_adcrowdnet import AttnCanAdcrowdNet
 from .attn_can_adcrowdnet_freeze_vgg import AttnCanAdcrowdNetFreezeVgg
 from .attn_can_adcrowdnet_simple import AttnCanAdcrowdNetSimpleV1, AttnCanAdcrowdNetSimpleV2, AttnCanAdcrowdNetSimpleV3, AttnCanAdcrowdNetSimpleV4, AttnCanAdcrowdNetSimpleV5
-from .compact_cnn import CompactCNN, CompactCNNV2, CompactDilatedCNN, DefDilatedCCNN, DilatedCCNNv2
+from .compact_cnn import CompactCNN, CompactCNNV2, CompactDilatedCNN, DefDilatedCCNN, DilatedCCNNv2, CompactCNNV6
 from .my_ccnn import CustomCNNv1, CustomCNNv2, CustomCNNv3, CustomCNNv4
