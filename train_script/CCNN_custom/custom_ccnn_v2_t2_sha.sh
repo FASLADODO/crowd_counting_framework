f4c616536d13d4caf383166caae0d8298cb73738
Thai Thien <tthien@apcs.vn>
2020-03-28 11:17:37 +0700

    change gpu number

--- train_script/CCNN_custom/custom_ccnn_v2_t2_sha.sh
+++ train_script/CCNN_custom/custom_ccnn_v2_t2_sha.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_custom_compact_cnn.py  \
+CUDA_VISIBLE_DEVICES=3 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_custom_compact_cnn.py  \
 --task_id custom_ccnn_v2_t2_sha  \
 --note "train custom ccnn v1 branching 3 branchn no batchnorm, but with weight decay"  \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
