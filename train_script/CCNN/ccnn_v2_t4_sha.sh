36b5bbc17111dca87cfa082f5a512b84576c8fca
Thai Thien <tthien@apcs.vn>
2020-04-01 17:55:12 +0700

    t4 aug

---
+++ train_script/CCNN/ccnn_v2_t4_sha.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=2 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_compact_cnn.py  \
+--task_id ccnn_v2_t4_sha  \
+--note "ccnnv2 with decay and 20p augment"  \
+--input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
+--lr 1e-4 \
+--decay 1e-4 \
+--datasetname shanghaitech_p20 \
+--epochs 502 > logs/ccnn_v2_t4_sha.log  &
