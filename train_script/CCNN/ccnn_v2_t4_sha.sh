f7565a25dc7058d1f344cc0d6e610fa2bf8f19e9
Thai Thien <tthien@apcs.vn>
2020-04-01 17:58:38 +0700

    fix typo

--- train_script/CCNN/ccnn_v2_t4_sha.sh
+++ train_script/CCNN/ccnn_v2_t4_sha.sh
@@ -4,5 +4,5 @@ CUDA_VISIBLE_DEVICES=2 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_co
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
 --lr 1e-4 \
 --decay 1e-4 \
---datasetname shanghaitech_p20 \
+--datasetname shanghaitech_20p \
 --epochs 502 > logs/ccnn_v2_t4_sha.log  &
