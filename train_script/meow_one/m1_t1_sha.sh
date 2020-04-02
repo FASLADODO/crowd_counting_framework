3e12138f4effbbb0fb8dc1e5aa98c7f8a689d737
Thai Thien <tthien@apcs.vn>
2020-04-02 17:34:01 +0700

    typo again

--- train_script/meow_one/m1_t1_sha.sh
+++ train_script/meow_one/m1_t1_sha.sh
@@ -1,9 +1,11 @@
+task="m1_t1_sha"
+
 CUDA_VISIBLE_DEVICES=1 HTTPS_PROXY="http://10.30.58.36:81" nohup python experiment_meow_main.py  \
---task_id m1_t1_sha  \
+--task_id $task  \
 --note "m1 sha shanghaitech_keepfull with decay, change the head, keep the tail "  \
 --model "M1" \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
 --lr 1e-4 \
 --decay 1e-4 \
 --datasetname shanghaitech_keepfull \
---epochs 502 > logs/custom_ccnn_v2_t2_sha.log  &
+--epochs 502 > logs/$task.log  &
