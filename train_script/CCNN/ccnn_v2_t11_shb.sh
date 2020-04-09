331644c623b1bf5a34c4db432e1526f6bc34a398
Thai Thien <tthien@apcs.vn>
2020-04-10 00:04:49 +0700

    11

---
+++ train_script/CCNN/ccnn_v2_t11_shb.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=3 HTTPS_PROXY="http://10.30.58.36:81" nohup python train_compact_cnn.py  \
+--task_id ccnn_v2_t11_shb  \
+--note "ccnnv2 with decay, 1e-5 lr "  \
+--input /data/rnd/thient/thient_data/ShanghaiTech/part_B  \
+--lr 1e-5 \
+--decay 1e-5 \
+--batch_size 24 \
+--datasetname shanghaitech_rnd \
+--epochs 601 > logs/ccnn_v2_t11_shb.log  &
