358f164d558dab393f65c0829d8d9c37b1437ff3
Thai Thien <tthien@apcs.vn>
2020-04-16 21:32:49 +0700

    fix url

--- train_script/meow_one/big_tail/bigtail3_t1_shb_sha.sh
+++ train_script/meow_one/big_tail/bigtail3_t1_shb_sha.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=1 HTTPS_PROXY="http://10.60.28.99:86" nohup python experime
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
 --lr 1e-4 \
 --decay 1e-4 \
---load_model saved_model/bigtail3_t1_sha/bigtail3_t1_shb_checkpoint_60000.pth \
+--load_model saved_model/bigtail3_t1_shb/bigtail3_t1_shb_checkpoint_60000.pth \
 --datasetname shanghaitech_20p \
 --epochs 1002 > logs/$task.log  &
 
