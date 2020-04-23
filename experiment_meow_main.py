36a2603484395ed130dd7dcb69c98c7057adf3ec
Thai Thien <tthien@apcs.vn>
2020-04-24 00:21:51 +0700

    turn off debug

--- experiment_meow_main.py
+++ experiment_meow_main.py
@@ -17,8 +17,8 @@ import os
 from model_util import get_lr
 
 COMET_ML_API = "S3mM1eMq6NumMxk2QJAXASkUM"
-# PROJECT_NAME = "meow-one-experiment-insita"
-PROJECT_NAME = "crowd-counting-debug"
+PROJECT_NAME = "meow-one-experiment-insita"
+# PROJECT_NAME = "crowd-counting-debug"
 
 
 def very_simple_param_count(model):
