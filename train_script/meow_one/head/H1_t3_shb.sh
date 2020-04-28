d37516f53d21f3dfc05143ba5ffda80fc5a07825
Thai Thien <tthien@apcs.vn>
2020-04-29 00:56:57 +0700

    2500

--- train_script/meow_one/head/H1_t3_shb.sh
+++ train_script/meow_one/head/H1_t3_shb.sh
@@ -10,6 +10,6 @@ CUDA_VISIBLE_DEVICES=1 HTTPS_PROXY="http://10.60.28.99:86" nohup python experime
 --batch_size 8 \
 --load_model saved_model/H1_t2_sha/H1_t2_sha_checkpoint_840000.pth \
 --datasetname shanghaitech_rnd \
---epochs 2001 > logs/$task.log  &
+--epochs 2501 > logs/$task.log  &
 
 echo logs/$task.log  # for convenience
