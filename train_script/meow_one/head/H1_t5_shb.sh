ca07533d585def04eec086685f2e72eacc330ddc
Thai Thien <tthien@apcs.vn>
2020-04-29 23:59:47 +0700

    delete misinformation note

--- train_script/meow_one/head/H1_t5_shb.sh
+++ train_script/meow_one/head/H1_t5_shb.sh
@@ -2,7 +2,7 @@ task="H1_t5_shb"
 
 CUDA_VISIBLE_DEVICES=3 HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_meow_main.py  \
 --task_id $task  \
---note "a continue train from sha"  \
+--note "a"  \
 --model "H1" \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_B  \
 --lr 2e-5 \
