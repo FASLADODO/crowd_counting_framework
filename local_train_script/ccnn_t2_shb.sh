1d4e08754bd493e76cf09076de7c76adf1a30a0a
Thai Thien <tthien@apcs.vn>
2020-04-30 19:10:15 +0700

    local

--- local_train_script/ccnn_t2_shb.sh
+++ local_train_script/ccnn_t2_shb.sh
@@ -2,9 +2,10 @@ task="local_ccnn_t2_shb"
 
 python train_compact_cnn.py \
 --task_id $task  \
---note ""  \
+--note "empty"  \
 --model "CompactCNNV7" \
 --input /data/ShanghaiTech/part_B  \
+--use_ssim \
 --lr 1e-4 \
 --decay 1e-4 \
 --batch_size 8 \
