edf1dcc13c83aaa933d94ea77140e551c0660e14
Thai Thien <tthien@apcs.vn>
2020-05-01 02:41:33 +0700

    ssim t6

--- train_script/CCNN/ssim_exp/ccnn_v7_ssim_t6_shb.sh
+++ train_script/CCNN/ssim_exp/ccnn_v7_ssim_t6_shb.sh
@@ -1,6 +1,6 @@
 task="ccnn_v7_ssim_t6_shb"
 
-CUDA_VISIBLE_DEVICES=3 HTTPS_PROXY="http://10.60.28.99:86" nohup python train_compact_cnn.py  \
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=4 HTTPS_PROXY="http://10.60.28.99:86" nohup python train_compact_cnn.py  \
 --task_id $task  \
 --note "1e-5 shanghaitech_rnd"  \
 --model "CompactCNNV7" \
