ea5737c694cb2967cb041db99ca391d06a66100d
Thai Thien <tthien@apcs.vn>
2020-05-02 18:19:18 +0700

    batch size 20

--- train_script/CCNN/sha_fixed/ccnn_v7_t2_shb_fixed.sh
+++ train_script/CCNN/sha_fixed/ccnn_v7_t2_shb_fixed.sh
@@ -7,6 +7,7 @@ CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=5 HTTPS_PROXY="http://10.60.28.99:86" noh
 --input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_fixed_sigma/part_B  \
 --lr 1e-4 \
 --decay 1e-4 \
+--batch_size 20 \
 --datasetname shanghaitech_keepfull \
 --epochs 901 > logs/$task.log  &
 
