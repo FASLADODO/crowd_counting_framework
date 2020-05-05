da020f46703ca4fae867a09960593ef6818b4a91
Thai Thien <tthien@apcs.vn>
2020-05-06 00:05:06 +0700

    train
    H1_t7_shb_fixed
    bigtail3 t3 sha shb fixed

---
+++ train_script/meow_one/head/H1_t7_shb_fixed.sh
@@ -0,0 +1,14 @@
+task="H1_t7_shb_fixed"
+
+CUDA_VISIBLE_DEVICES=1 HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_meow_main.py  \
+--task_id $task  \
+--note "shb sigma 15"  \
+--model "H1" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_fixed_sigma/part_B  \
+--lr 1e-4 \
+--decay 1e-4 \
+--batch_size 20 \
+--datasetname shanghaitech_rnd \
+--epochs 1001 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
