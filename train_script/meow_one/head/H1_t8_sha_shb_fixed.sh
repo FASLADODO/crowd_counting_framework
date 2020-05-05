1c692b37536bd72abaa0995001d3a396b82bc2f0
Thai Thien <tthien@apcs.vn>
2020-05-06 00:24:56 +0700

    H1 t8

---
+++ train_script/meow_one/head/H1_t8_sha_shb_fixed.sh
@@ -0,0 +1,15 @@
+task="H1_t8_sha_shb_fixed"
+
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=5 HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_meow_main.py  \
+--task_id $task  \
+--note "shb sigma 15 load trained sha"  \
+--model "H1" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_fixed_sigma/part_B  \
+--lr 1e-4 \
+--decay 1e-4 \
+--batch_size 20 \
+--load_model saved_model/H1_t2_sha/ H1_t2_sha_checkpoint_840000.pth  \
+--datasetname shanghaitech_rnd \
+--epochs 1001 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
