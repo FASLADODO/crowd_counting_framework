e67e3fcb8b77db53f990f8582e43c053f69d6c09
Thai Thien <tthien@apcs.vn>
2020-05-22 00:31:12 +0700

    train

---
+++ train_script/meow_one/head/H1_t10_sha.sh
@@ -0,0 +1,14 @@
+task="H1_t9_sha"
+
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=5 HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "shb sigma 15 L1"  \
+--model "H1" \
+--input /data/rnd/thient/thient_data/ShanghaiTech/part_A   \
+--lr 1e-4 \
+--decay 1e-4 \
+--loss_fn "L1" \
+--datasetname shanghaitech_20p \
+--epochs 601 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
---
+++ train_script/meow_one/head/H2_t2_shb_fixed.sh
@@ -0,0 +1,15 @@
+task="H2_t1_shb_fixed"
+
+CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=5 HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "shb sigma 15"  \
+--model "H2" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_fixed_sigma/part_B  \
+--lr 1e-4 \
+--decay 1e-4 \
+--loss_fn "L1" \
+--batch_size 20 \
+--datasetname shanghaitech_rnd \
+--epochs 601 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
