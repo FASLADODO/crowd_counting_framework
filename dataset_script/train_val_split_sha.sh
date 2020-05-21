e864e72a9b1fd0e6dfe200b3a9b569016f9894c0
Thai Thien <tthien@apcs.vn>
2020-05-22 00:24:38 +0700

    fix script

--- dataset_script/train_val_split_sha.sh
+++ dataset_script/train_val_split_sha.sh
@@ -1,3 +1,3 @@
 python dataset_script/sht_train_validate_split.py \
---input /data/rnd/thient/thient_data/ShanghaiTech/part_A
+--input /data/rnd/thient/thient_data/ShanghaiTech/part_A/train_data
 
--- dataset_script/train_val_split_shb.sh
+++ dataset_script/train_val_split_shb.sh
@@ -1,2 +1,2 @@
 python dataset_script/sht_train_validate_split.py \
---input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_fixed_sigma/part_B
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_fixed_sigma/part_B/train_data
