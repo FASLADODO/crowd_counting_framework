82806340334b3beb9916b34bb00a66381ce57aae
Thai Thien <ttpro1995@yahoo.com.vn>
2020-05-26 19:00:49 +0700

    surpress python warning

--- train_script/CCNN/ccnn_v7_t17_sha.sh
+++ train_script/CCNN/ccnn_v7_t17_sha.sh
@@ -1,6 +1,6 @@
 task="ccnn_v7_t17_sha"
 
-CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=5 HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=5 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
 --task_id $task  \
 --note "MSE mean with sha"  \
 --model "CompactCNNV7" \
