0a2cf847de58fc032ee966632af94cdef1f16245
Thai Thien <ttpro1995@yahoo.com.vn>
2020-06-05 16:36:40 +0700

    bigtail4 56

---
+++ train_script/meow_one/big_tail/bigtail4_t6_sha.sh
@@ -0,0 +1,15 @@
+task="bigtail4_t6_sha"
+
+CUDA_VISIBLE_DEVICES=6 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "bigtail4"  \
+--model "BigTail4" \
+--input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
+--lr 2e-5 \
+--decay 1e-4  \
+--loss_fn "MSE" \
+--skip_train_eval \
+--datasetname shanghaitech_crop_random \
+--epochs 800 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
