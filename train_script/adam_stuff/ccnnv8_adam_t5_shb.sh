4e8e6e864c3f2fea80a3b61addbc26eb8f3ae059
Thai Thien <tthien@apcs.vn>
2020-06-11 22:47:04 +0700

    ccnnv8

--- train_script/adam_stuff/ccnnv8_adam_t5_shb.sh
+++ train_script/adam_stuff/ccnnv8_adam_t5_shb.sh
@@ -3,7 +3,7 @@ task="ccnn_adam_t3_shb"
 CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
 --task_id $task  \
 --note "adam lr and decay, 8"  \
---model "CompactCNNV7" \
+--model "CompactCNNV8" \
 --input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_fixed_sigma/part_B  \
 --lr 1e-5 \
 --decay 1e-5  \
