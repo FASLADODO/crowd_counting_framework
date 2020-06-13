07c6055e3bdacaca34c0aaacd49d5c87a07f4320
Thai Thien <tthien@apcs.vn>
2020-06-13 11:44:20 +0700

    typo in training script
    ccnnv9_adam_t3_sha
    ccnnv9_adam_t4_sha

--- train_script/adam_stuff/ccnnv9_adam_t4_sha.sh
+++ train_script/adam_stuff/ccnnv9_adam_t4_sha.sh
@@ -1,13 +1,13 @@
-task="ccnnv9_adam_t1_sha"
+task="ccnnv9_adam_t4_sha"
 
-CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+CUDA_VISIBLE_DEVICES=5 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
 --task_id $task  \
 --note "leaky relu"  \
---model "CompactCNNV8" \
+--model "CompactCNNV9" \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
 --lr 1e-4 \
 --decay 1e-4  \
---loss_fn "MSEMean" \
+--loss_fn "L1Mean" \
 --skip_train_eval \
 --batch_size 1 \
 --optim  "adam" \
