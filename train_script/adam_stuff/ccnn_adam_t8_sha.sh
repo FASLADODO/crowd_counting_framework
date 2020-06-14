7e6984a9eaa6609182ad6c786f6742dd29f1d017
Thai Thien <tthien@apcs.vn>
2020-06-14 17:23:05 +0700

    train script
    ccnn_adam_t8_sha

---
+++ train_script/adam_stuff/ccnn_adam_t8_sha.sh
@@ -0,0 +1,17 @@
+task="ccnn_adam_t8_sha"
+
+CUDA_VISIBLE_DEVICES=5 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "a"  \
+--model "CompactCNNV7" \
+--input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
+--lr 1e-5 \
+--decay 1e-5  \
+--loss_fn "MSEMean" \
+--skip_train_eval \
+--batch_size 1 \
+--optim  "adam" \
+--datasetname shanghaitech_crop_random \
+--epochs 1200 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
