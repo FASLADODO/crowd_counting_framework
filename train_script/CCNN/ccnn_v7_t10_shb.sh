2f443c077e186901cdbe2c9faf27d2f37905a775
Thai Thien <tthien@apcs.vn>
2020-07-13 01:57:39 +0700

    t10

--- train_script/CCNN/ccnn_v7_t10_shb.sh
+++ train_script/CCNN/ccnn_v7_t10_shb.sh
@@ -1,6 +1,6 @@
 task="ccnn_v7_t10_shb"
 
-CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=3 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=3 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
 --task_id $task  \
 --note "try new argmentation and train test split "  \
 --model "CompactCNNV7" \
