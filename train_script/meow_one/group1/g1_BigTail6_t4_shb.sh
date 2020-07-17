34c120b57c41fe7e33af2a0e6bb59a63467ec2f6
Thai Thien <tthien@apcs.vn>
2020-07-18 02:48:00 +0700

    t4

---
+++ train_script/meow_one/group1/g1_BigTail6_t4_shb.sh
@@ -0,0 +1,18 @@
+task="g1_BigTail6_t4_shb"
+
+CUDA_VISIBLE_DEVICES=6 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "try SGD instead of adam, lr -6"  \
+--model "BigTail6" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_B  \
+--lr 1e-6 \
+--decay 1e-6 \
+--batch_size 4 \
+--loss_fn "MSEL1Sum" \
+--datasetname shanghaitech_non_overlap \
+--skip_train_eval \
+--cache \
+--optim sgd \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
