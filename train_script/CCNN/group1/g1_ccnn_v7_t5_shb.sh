35192dfa0012d099ba5d8515e125a2c977503cd6
Thai Thien <ttpro1995@yahoo.com.vn>
2020-07-17 14:37:41 +0700

    v7 t567

---
+++ train_script/CCNN/group1/g1_ccnn_v7_t5_shb.sh
@@ -0,0 +1,18 @@
+task="g1_ccnn_v7_t5_shb"
+
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "valid mae fluctuation, try increase batch size"  \
+--model "CompactCNNV7" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_B  \
+--lr 1e-4 \
+--decay 1e-4 \
+--batch_size 5 \
+--loss_fn "MSEL1Sum" \
+--datasetname shanghaitech_non_overlap \
+--skip_train_eval \
+--cache \
+--pin_memory  \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
---
+++ train_script/CCNN/group1/g1_ccnn_v7_t7_shb.sh
@@ -0,0 +1,18 @@
+task="g1_ccnn_v7_t7_shb"
+
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "valid mae fluctuation, try increase batch size more, reduce lr"  \
+--model "CompactCNNV7" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_B  \
+--lr 5e-5 \
+--decay 1e-4 \
+--batch_size 6 \
+--loss_fn "MSEL1Sum" \
+--datasetname shanghaitech_non_overlap \
+--skip_train_eval \
+--cache \
+--pin_memory  \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
