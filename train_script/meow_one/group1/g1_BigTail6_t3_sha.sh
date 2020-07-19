ebc66b56af0fe4c86ec4a8c3a8b6b63432aabe9a
Thai Thien <tthien@apcs.vn>
2020-07-19 21:10:08 +0700

    gpu4

--- train_script/meow_one/group1/g1_BigTail6_t3_sha.sh
+++ train_script/meow_one/group1/g1_BigTail6_t3_sha.sh
@@ -1,6 +1,6 @@
 task="g1_BigTail6_t3_sha"
 
-CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
 --task_id $task  \
 --note "mse l1 sum, with -5 lr and decay"  \
 --model "BigTail6" \
