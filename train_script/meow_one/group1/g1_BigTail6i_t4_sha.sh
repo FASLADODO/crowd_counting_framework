7c7e0815e85129cbe00d735f81f4e909a57ac6cd
Thai Thien <tthien@apcs.vn>
2020-07-21 00:04:38 +0700

    flip only

--- train_script/meow_one/group1/g1_BigTail6i_t4_sha.sh
+++ train_script/meow_one/group1/g1_BigTail6i_t4_sha.sh
@@ -2,13 +2,13 @@ task="g1_BigTail6i_t4_sha"
 
 CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
 --task_id $task  \
---note "mse l1 sum, with -5 lr and decay"  \
+--note "mse l1 sum, with -4 lr and decay, flip only"  \
 --model "BigTail6i" \
 --input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_A  \
---lr 1e-5 \
---decay 1e-5 \
+--lr 1e-4 \
+--decay 1e-4 \
 --loss_fn "MSEL1Sum" \
---datasetname shanghaitech_non_overlap \
+--datasetname shanghaitech_flip_only \
 --skip_train_eval \
 --cache \
 --epochs 1201 > logs/$task.log  &
