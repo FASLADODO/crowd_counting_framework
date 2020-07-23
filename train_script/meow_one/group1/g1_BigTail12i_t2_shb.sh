cdfeca43979abf6984df019c76268f5676bbf258
Thai Thien <ttpro1995@yahoo.com.vn>
2020-07-23 20:47:16 +0700

    t2

---
+++ train_script/meow_one/group1/g1_BigTail12i_t2_shb.sh
@@ -0,0 +1,17 @@
+task="g1_BigTail12i_t2_shb"
+
+CUDA_VISIBLE_DEVICES=6 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "mse l1 sum, with -4 lr and -2 decay (help overfit), use batchnorm (default setting)"  \
+--model "BigTail12i" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_B  \
+--lr 1e-3 \
+--decay 1e-2 \
+--loss_fn "MSEL1Sum" \
+--batch_size 5 \
+--datasetname shanghaitech_non_overlap \
+--skip_train_eval \
+--cache \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
