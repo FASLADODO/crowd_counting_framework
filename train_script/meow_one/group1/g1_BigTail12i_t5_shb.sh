0bf16031393b428de979b362f1806ae245e3fdce
Thai Thien <tthien@apcs.vn>
2020-07-24 22:05:02 +0700

    5

--- train_script/meow_one/group1/g1_BigTail12i_t5_shb.sh
+++ train_script/meow_one/group1/g1_BigTail12i_t5_shb.sh
@@ -1,6 +1,6 @@
 task="g1_BigTail12i_t5_shb"
 
-CUDA_VISIBLE_DEVICES=6 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+CUDA_VISIBLE_DEVICES=5 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
 --task_id $task  \
 --note "mse l1 sum, sgd, use batchnorm (default setting)"  \
 --model "BigTail12i" \
