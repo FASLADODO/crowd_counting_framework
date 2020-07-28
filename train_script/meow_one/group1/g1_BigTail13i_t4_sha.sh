6f47f27384806e573704585a0a84ee1d23694dca
Thai Thien <tthien@apcs.vn>
2020-07-28 22:17:08 +0700

    a

---
+++ train_script/meow_one/group1/g1_BigTail13i_t4_sha.sh
@@ -0,0 +1,16 @@
+task="g1_BigTail13i_t4_sha"
+
+CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "mse l1 sum, nonoverlab on A, high decay"  \
+--model "BigTail13i" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_A  \
+--lr 1e-4 \
+--decay 1e-2 \
+--loss_fn "MSEL1Sum" \
+--datasetname shanghaitech_non_overlap \
+--skip_train_eval \
+--cache \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
