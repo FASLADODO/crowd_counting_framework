d3981fdc091b7654fd073f95dffb6d27542aeb68
Thai Thien <tthien@apcs.vn>
2020-07-29 23:13:01 +0700

    sha experiment

---
+++ train_script/meow_one/group1/g1_BigTail13i_t5_sha.sh
@@ -0,0 +1,16 @@
+task="g1_BigTail13i_t5_sha"
+
+CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "mse l1 sum, with -4 lr and -2 decay (help overfit), flip only"  \
+--model "BigTail13i" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_A  \
+--lr 1e-4 \
+--decay 1e-2 \
+--loss_fn "MSEL1Sum" \
+--datasetname shanghaitech_flip_only \
+--skip_train_eval \
+--cache \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
---
+++ train_script/meow_one/group1/g1_BigTail14i_t1_sha.sh
@@ -0,0 +1,16 @@
+task="g1_BigTail14i_t1_sha"
+
+CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "mse l1 sum, with -4 lr and -2 decay (help overfit), flip only"  \
+--model "BigTail14i" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_A  \
+--lr 1e-4 \
+--decay 1e-2 \
+--loss_fn "MSEL1Sum" \
+--datasetname shanghaitech_flip_only \
+--skip_train_eval \
+--cache \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
