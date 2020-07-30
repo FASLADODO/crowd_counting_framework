8787c9eaceaec5f1199ec8092fd5682843697b5d
Thai Thien <tthien@apcs.vn>
2020-07-31 00:42:31 +0700

    t3

---
+++ train_script/meow_one/group1/g1_BigTail15i_t3_sha.sh
@@ -0,0 +1,17 @@
+task="g1_BigTail15i_t2_sha"
+
+CUDA_VISIBLE_DEVICES=6 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "mse l1 sum, with -5 lr and -5 decay sgd, flip only"  \
+--model "BigTail15i" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_A  \
+--lr 1e-5 \
+--decay 1e-5 \
+--loss_fn "MSEL1Mean" \
+--datasetname shanghaitech_flip_only \
+--skip_train_eval \
+--cache \
+--optim sgd \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
