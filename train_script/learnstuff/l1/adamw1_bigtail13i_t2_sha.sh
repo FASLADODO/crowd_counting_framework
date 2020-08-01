8bc23f1401eacdc2e28f30a825503f0dd96bacc9
Thai Thien <tthien@apcs.vn>
2020-08-01 22:59:29 +0700

    adamw1_bigtail13i_t2_sha

---
+++ train_script/learnstuff/l1/adamw1_bigtail13i_t2_sha.sh
@@ -0,0 +1,16 @@
+task="adamw1_bigtail13i_t2_sha"
+
+CUDA_VISIBLE_DEVICES=6 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "adamW with -3 lr and decay, msel1mean"  \
+--model "BigTail13i" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_A  \
+--lr 1e-3 \
+--decay 1e-3 \
+--loss_fn "MSEL1Mean" \
+--datasetname shanghaitech_crop_random \
+--optim adamw \
+--cache \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
