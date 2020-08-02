56048b30fb4a19ae52c641b07754756f43ddcefb
Thai Thien <tthien@apcs.vn>
2020-08-02 11:00:46 +0700

    g

--- train_script/learnstuff/l1/adamw2_bigtail13i_t1_sha.sh
+++ train_script/learnstuff/l1/adamw2_bigtail13i_t1_sha.sh
@@ -1,6 +1,6 @@
 task="adamw2_bigtail13i_t1_sha"
 
-CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
 --task_id $task  \
 --note "adamW with -3 lr and -5 decay (small decay for small batch size) , msel1mean"  \
 --model "BigTail13i" \
