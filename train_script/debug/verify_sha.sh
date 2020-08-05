8148d61da1c8eca9e4b3a4b24ddd6d44db069f0f
Thai Thien <tthien@apcs.vn>
2020-08-06 00:02:27 +0700

    logs with s again

--- train_script/debug/verify_sha.sh
+++ train_script/debug/verify_sha.sh
@@ -2,5 +2,5 @@ nohup python debug/verify_sha.py \
 --input  /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_A/train_data_train_split \
 --output viz/verify_sha_3_train_split_shanghaitech_keepfull_r50 \
 --dataset  shanghaitech_keepfull_r50  \
---meta_data  log/verify_sha_3_train_split_shanghaitech_keepfull_r50.csv  \
+--meta_data  logs/verify_sha_3_train_split_shanghaitech_keepfull_r50.csv  \
 > logs/verify_sha_3_train_split_shanghaitech_keepfull_r50.log &
