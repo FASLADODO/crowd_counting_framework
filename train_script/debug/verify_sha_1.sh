88550cb75ac83c5443b0a5e5f805f0c6cc05cfde
Thai Thien <ttpro1995@yahoo.com.vn>
2020-08-08 16:59:21 +0700

    let verify sha_1 to see how it is doing

---
+++ train_script/debug/verify_sha_1.sh
@@ -0,0 +1,6 @@
+nohup python debug/verify_sha.py \
+--input  /data/rnd/thient/thient_data/ShanghaiTech/part_A/train_data_train_split \
+--output viz/verify_sha_1_train_split_shanghaitech_keepfull_r50 \
+--dataset  shanghaitech_keepfull_r50  \
+--meta_data  logs/verify_sha_1_train_split_shanghaitech_keepfull_r50.csv  \
+> logs/verify_sha_1_train_split_shanghaitech_keepfull_r50.log &
