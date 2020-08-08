5d240b5c9ea5dd91e149b1b7652e1077d1158e39
Thai Thien <tthien@apcs.vn>
2020-08-09 01:02:30 +0700

    l2_adamw2_bigtail11i_t1_sha

---
+++ train_script/learnstuff/l2/l2_adamw2_bigtail11i_t1_sha.sh
@@ -0,0 +1,21 @@
+task="l2_adamw2_bigtail11i_t1_sha"
+
+CUDA_VISIBLE_DEVICES=7 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "11i no batchnorm keepfull with lr scheduler, starting -3"  \
+--model "BigTail11i" \
+--input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
+--lr 1e-3 \
+--lr_scheduler \
+--step_list 30,50,70 \
+--lr_list 1e-3,1e-4,1e-5 \
+--decay 1e-5 \
+--loss_fn "MSEL1Mean" \
+--datasetname shanghaitech_keepfull_r50 \
+--optim adamw \
+--cache \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
+
+#shanghaitech_keepfull_r50
