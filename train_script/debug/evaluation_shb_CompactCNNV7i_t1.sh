bb1e40fc7806c8bef5e94fbaa54ac9af3b599041
Thai Thien <tthien@apcs.vn>
2020-08-23 15:23:30 +0700

    test set

--- train_script/debug/evaluation_shb_CompactCNNV7i_t1.sh
+++ train_script/debug/evaluation_shb_CompactCNNV7i_t1.sh
@@ -1,7 +1,8 @@
 task="evaluation_shb_CompactCNNV7i_t1"
 CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=4 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python debug/evaluate_shb.py  \
 --model "CompactCNNV7i" \
---input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_B  \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_B/test_data  \
+--output visualize/$task  \
 --load_model  saved_model_best/g1_ccnn_v7_t3_shb/g1_ccnn_v7_t3_shb_checkpoint_valid_mae=-8.881268501281738.pth  \
 --meta_data logs/$task.txt  \
 --datasetname shanghaitech_non_overlap \
