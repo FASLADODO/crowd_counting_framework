e8aebcfb782966c11c3ca116b5a9cc254021a73d
Thai Thien <tthien@apcs.vn>
2020-08-23 15:41:02 +0700

    TypeError: can only concatenate str (not "list") to str

--- debug/evaluate_shb.py
+++ debug/evaluate_shb.py
@@ -61,6 +61,7 @@ def visualize_evaluation_shanghaitech_keepfull(model, args):
             img, gt_density, debug_info = item
             gt_count = debug_info["p_count"]
             file_name = debug_info["name"]
+            print(file_name)
             save_path = os.path.join(saved_folder, "label_" + file_name +".png")
             save_pred_path = os.path.join(saved_folder, "pred_" + file_name +".png")
             save_density_map(gt_density.numpy()[0][0], save_path)
