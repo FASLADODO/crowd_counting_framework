5e6605625ba130310fbeaade259e4a5b98987dad
Thai Thien <tthien@apcs.vn>
2020-09-08 00:04:43 +0700

    change data path

--- dataset_script/jhucrowd_test.sh
+++ dataset_script/jhucrowd_test.sh
@@ -1,6 +1,6 @@
 HTTPS_PROXY="http://10.60.28.99:86" nohup python dataset_script/jhucrowd_density_map.py \
 --task_id jhu_test \
---input /data/rnd/thient/thient_data/jhu_crowd_plusplus/test \
+--input /data/rnd/thient/thient_data/jhu_crowd_plusplus/test_data \
  > logs/jhucrowd_density_map_test_t2.log  &
 
  echo logs/jhucrowd_density_map_test_t2.log
