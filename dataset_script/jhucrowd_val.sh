7475799e0ff4bdc51ce86ca98751535c841d49c9
Thai Thien <tthien@apcs.vn>
2020-09-07 23:52:00 +0700

    generate dataset again

--- dataset_script/jhucrowd_val.sh
+++ dataset_script/jhucrowd_val.sh
@@ -1,4 +1,4 @@
 nohup python dataset_script/jhucrowd_density_map.py \
 --task_id val \
 --input /data/rnd/thient/thient_data/jhu_crowd_plusplus/val \
- > logs/jhucrowd_density_map_val.log  &
+ > logs/jhucrowd_density_map_val_t2.log  &
