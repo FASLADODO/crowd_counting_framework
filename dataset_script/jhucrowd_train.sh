5a43d57ba16667cb96f9514dcf4b81167ff8bd5a
Thai Thien <tthien@apcs.vn>
2020-09-08 22:01:51 +0700

    val train test

--- dataset_script/jhucrowd_train.sh
+++ dataset_script/jhucrowd_train.sh
@@ -1,6 +1,6 @@
 HTTPS_PROXY="http://10.60.28.99:86" nohup python dataset_script/jhucrowd_density_map.py \
 --task_id jhu_train \
 --input /data/rnd/thient/thient_data/jhu_crowd_plusplus/train_data_train_split \
- > logs/jhucrowd_density_map_train_t2.log  &
+ > logs/jhucrowd_density_map_train_t3.log  &
 
- echo logs/jhucrowd_density_map_train_t2.log
+ echo logs/jhucrowd_density_map_train_t3.log
--- dataset_script/jhucrowd_val.sh
+++ dataset_script/jhucrowd_val.sh
@@ -1,6 +1,6 @@
 HTTPS_PROXY="http://10.60.28.99:86" nohup python dataset_script/jhucrowd_density_map.py \
 --task_id jhu_val \
 --input /data/rnd/thient/thient_data/jhu_crowd_plusplus/train_data_validate_split \
- > logs/jhucrowd_density_map_val_t2.log  &
+ > logs/jhucrowd_density_map_val_t3.log  &
 
- echo logs/jhucrowd_density_map_val_t2.log
+ echo logs/jhucrowd_density_map_val_t3.log
