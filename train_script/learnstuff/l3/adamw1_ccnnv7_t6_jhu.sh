1fa1d544a254cd042bcdd36e8de2984fe3d5244f
Thai Thien <tthien@apcs.vn>
2020-09-17 00:56:15 +0700

    adamw1_CompactCNNV7_t6_jhu

---
+++ train_script/learnstuff/l3/adamw1_ccnnv7_t6_jhu.sh
@@ -0,0 +1,17 @@
+task="adamw1_CompactCNNV7_t6_jhu"
+
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=6 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "lower lr and decay"  \
+--model "CompactCNNV7" \
+--input /data/rnd/thient/thient_data/jhu_crowd_plusplus  \
+--lr 1e-5 \
+--decay 0.001 \
+--loss_fn "MSEL1Mean" \
+--batch_size 60 \
+--datasetname jhucrowd_downsample_512 \
+--optim adamw \
+--skip_train_eval \
+--epochs 401 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
