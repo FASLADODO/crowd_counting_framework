7fd22510845dec808100058190a202b805bf6a02
Thai Thien <tthien@apcs.vn>
2020-09-23 21:17:00 +0700

    l2_adamw2_bigtail13i_t13_sha

---
+++ train_script/learnstuff/l2/l2_adamw2_bigtail13i_t13_sha.sh
@@ -0,0 +1,18 @@
+task="l2_adamw2_bigtail13i_t13_sha"
+
+CUDA_VISIBLE_DEVICES=5 OMP_NUM_THREADS=6 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "256  5e-4 lr and decay -3. decrease lr to see if it go higher"  \
+--model "BigTail13i" \
+--input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
+--lr 5e-4 \
+--decay 1e-3 \
+--loss_fn "MSEL1Mean" \
+--datasetname shanghaitech_256_v2 \
+--optim adamw \
+--batch_size 90  \
+--epochs 3001 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
+
+#shanghaitech_keepfull_r50
