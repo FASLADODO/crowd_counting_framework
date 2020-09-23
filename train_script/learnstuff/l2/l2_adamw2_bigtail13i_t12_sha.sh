bf67476db55f55f348068bc7683f68834659cc96
Thai Thien <tthien@apcs.vn>
2020-09-23 21:14:35 +0700

    l2_adamw2_bigtail13i_t12_sha

--- train_script/learnstuff/l2/l2_adamw2_bigtail13i_t12_sha.sh
+++ train_script/learnstuff/l2/l2_adamw2_bigtail13i_t12_sha.sh
@@ -1,20 +1,18 @@
-task="l2_adamw2_bigtail13i_t7_sha"
+task="l2_adamw2_bigtail13i_t12_sha"
 
 CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=4 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
 --task_id $task  \
---note "256 lr and decay -3"  \
+--note "256  5e-4 lr and decay -3. decrease lr to see if it go higher"  \
 --model "BigTail13i" \
 --input /data/rnd/thient/thient_data/ShanghaiTech/part_A  \
---lr 1e-3 \
+--lr 5e-4 \
 --decay 1e-3 \
 --loss_fn "MSEL1Mean" \
 --datasetname shanghaitech_256_v2 \
 --optim adamw \
 --batch_size 60  \
---epochs 1201 > logs/$task.log  &
+--epochs 2001 > logs/$task.log  &
 
 echo logs/$task.log  # for convenience
 
 #shanghaitech_keepfull_r50
-
-# this give 86 mae
