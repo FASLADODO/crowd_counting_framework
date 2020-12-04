1a47e9caf6cc2e0bc54f850c70822b4ae346af11
Thai Thien <tthien@apcs.vn>
2020-12-05 02:30:53 +0700

    adamw1_csrnet_t6_shb

---
+++ train_script/learnstuff/c1/adamw1_csrnet_t6_shb.sh
@@ -0,0 +1,17 @@
+task="adamw1_csrnet_t6_shb"
+
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=2 PYTHONWARNINGS="ignore" HTTPS_PROXY="http://10.60.28.99:86" nohup python experiment_main.py  \
+--task_id $task  \
+--note "adamW csrnet on best shb of dccnn"  \
+--model "CSRNet" \
+--input /data/rnd/thient/thient_data/shanghaitech_with_people_density_map/ShanghaiTech_3/part_B  \
+--lr 1e-7 \
+--decay 0.0005 \
+--loss_fn "MSEL1Mean" \
+--batch_size 2 \
+--datasetname shanghaitech_non_overlap \
+--optim adamw \
+--cache \
+--epochs 1201 > logs/$task.log  &
+
+echo logs/$task.log  # for convenience
